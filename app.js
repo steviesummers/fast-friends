433a0ece6ff1d56d064b213ddb4e3adbf72e3337
steviesummers <steviesummerss@gmail.com>
2024-02-18 14:38:09 -0800

    connected to database

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const dotenv = require('dotenv').config()
 const port = 3001;
+const mongoUrl = process.env.MONGO_URL
 
 const server = app.listen(port, () => console.log("Server listening on Port: "+ port))
+
+mongoose.connect(mongoUrl).then(() => {
+    console.log("connected to DB successfully")
+}).catch((err) => {
+    console.log("Failed to connect to DB", err)
+})
